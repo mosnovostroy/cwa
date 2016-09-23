c0bddbbc47f8cbcce5e9c02c2a646ba4bda3afd2
mosnovostroy <inbox@mosnovostroy.ru>
2016-09-23 17:11:49 +0300

    1

--- common/models/User.php
+++ common/models/User.php
@@ -409,7 +409,7 @@ class User extends ActiveRecord implements IdentityInterface
             fclose($fp);
 
             // Генерируем 50х50 превью аватарки:
-            Image::thumbnail($filename, 50, 50) -> save($preview, ['quality' => 50]);
+            Image::thumbnail($file, 50, 50) -> save($preview, ['quality' => 50]);
           }
         }
     }
