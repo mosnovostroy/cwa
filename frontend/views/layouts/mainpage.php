2105c8602fbb70417fe638cd106acb6ab13307fe
mosnovostroy <inbox@mosnovostroy.ru>
2016-10-20 13:10:51 +0300

    1

--- frontend/views/layouts/mainpage.php
+++ frontend/views/layouts/mainpage.php
@@ -26,6 +26,35 @@ BootstrapSelectAsset::register($this);
     <?php if (isset($this->params['hasYandexMap']))
         echo '<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"> </script>';
     ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter40340380 = new Ya.Metrika({
+                        id:40340380,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/40340380" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+        
 </head>
 <body>
 <?php $this->beginBody() ?>
--- frontend/views/layouts/map.php
+++ frontend/views/layouts/map.php
@@ -26,6 +26,36 @@ BootstrapSelectAsset::register($this);
     <?php if (isset($this->params['hasYandexMap']))
         echo '<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"> </script>';
     ?>
+
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript">
+        (function (d, w, c) {
+            (w[c] = w[c] || []).push(function() {
+                try {
+                    w.yaCounter40340380 = new Ya.Metrika({
+                        id:40340380,
+                        clickmap:true,
+                        trackLinks:true,
+                        accurateTrackBounce:true
+                    });
+                } catch(e) { }
+            });
+
+            var n = d.getElementsByTagName("script")[0],
+                s = d.createElement("script"),
+                f = function () { n.parentNode.insertBefore(s, n); };
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+            if (w.opera == "[object Opera]") {
+                d.addEventListener("DOMContentLoaded", f, false);
+            } else { f(); }
+        })(document, window, "yandex_metrika_callbacks");
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/40340380" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <!-- /Yandex.Metrika counter -->
+        
 </head>
 <body>
 <?php $this->beginBody() ?>
@@ -36,7 +66,7 @@ BootstrapSelectAsset::register($this);
 	<?php $this->endContent(); ?>
 	<div class="container-map">
         <?= Alert::widget() ?>
-        <?=  $content ?>	
+        <?=  $content ?>
 	</div>
 </div>
 
@@ -47,6 +77,3 @@ BootstrapSelectAsset::register($this);
 </body>
 </html>
 <?php $this->endPage() ?>
-
-
-
