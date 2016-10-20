29621d3a935114cca922692f27d1b994397928c8
mosnovostroy <inbox@mosnovostroy.ru>
2016-10-20 16:40:27 +0300

    1

--- frontend/views/layouts/mainpage.php
+++ frontend/views/layouts/mainpage.php
@@ -70,6 +70,23 @@ BootstrapSelectAsset::register($this);
 
 </head>
 <body>
+
+    <!-- Rating@Mail.ru counter -->
+    <script type="text/javascript">
+    var _tmr = window._tmr || (window._tmr = []);
+    _tmr.push({id: "2827040", type: "pageView", start: (new Date()).getTime()});
+    (function (d, w, id) {
+      if (d.getElementById(id)) return;
+      var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+      ts.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//top-fwz1.mail.ru/js/code.js";
+      var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+      if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+    })(document, window, "topmailru-code");
+    </script><noscript><div style="position:absolute;left:-10000px;">
+    <img src="//top-fwz1.mail.ru/counter?id=2827040;js=na" style="border:0;" height="1" width="1" alt="Рейтинг@Mail.ru" />
+    </div></noscript>
+    <!-- //Rating@Mail.ru counter -->
+
 <?php $this->beginBody() ?>
 
 <?php $this->beginContent('@app/views/layouts/_nav.php'); ?>
@@ -88,15 +105,28 @@ BootstrapSelectAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">
-            &copy; Коворкинг-ревю <?= date('Y') ?>
-            &nbsp;&nbsp;&nbsp;
-            <?= Html::a('О проекте', ['site/about']) ?>
-            &nbsp;&nbsp;&nbsp;
-            <?= Html::a('Контакты', ['site/contacts']) ?>
-            &nbsp;&nbsp;&nbsp;
-            <?= Html::a('Реклама', ['site/adv']) ?>
-        </p>
+        <div class="row">
+            <div class="col-md-6">
+                <p class="pull-left">
+                    &copy; Коворкинг-ревю <?= date('Y') ?>
+                    &nbsp;&nbsp;&nbsp;
+                    <?= Html::a('О проекте', ['site/about']) ?>
+                    &nbsp;&nbsp;&nbsp;
+                    <?= Html::a('Контакты', ['site/contacts']) ?>
+                    &nbsp;&nbsp;&nbsp;
+                    <?= Html::a('Реклама', ['site/adv']) ?>
+                </p>
+            </div>
+            <div class="col-md-6">
+                <p class="pull-right">
+                    <!-- Rating@Mail.ru logo -->
+                    <a href="http://top.mail.ru/jump?from=2827040">
+                    <img src="//top-fwz1.mail.ru/counter?id=2827040;t=602;l=1"
+                    style="border:0;" height="40" width="88" alt="Рейтинг@Mail.ru" /></a>
+                    <!-- //Rating@Mail.ru logo -->
+                </p>
+            </div>
+        </div>
     </div>
 </footer>
 
