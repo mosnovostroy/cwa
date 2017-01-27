ba83c4a49f560afaa718e2a3677d0c6af1c8915d
mosnovostroy <inbox@mosnovostroy.ru>
2017-01-27 11:05:01 +0300

    1

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -51,60 +51,25 @@ $this->params['showCounters'] = true;
     </div>
 
     <div class="row">
+        <!-- <div class="col-sm-1">
+        </div> -->
         <?php $url = Url::to(['center/index']); ?>
-        <div class="col-sm-4" onclick="location.href='<?= $url ?>';">
+        <div class="col-sm-6" onclick="location.href='<?= $url ?>';">
             <div class="button-main clearfix">
                 <a href="<?=$url?>"><h4><p>Поиск коворкингов</p></h4></a>
             </div>
         </div>
+        <!-- <div class="col-sm-1">
+        </div> -->
         <?php $url = Url::to(['center/map']); ?>
-        <div class="col-sm-4" onclick="location.href='<?= $url ?>';">
+        <div class="col-sm-6" onclick="location.href='<?= $url ?>';">
             <div class="button-main clearfix">
                 <a href="<?=$url?>"><h4><p>Коворкинги на карте</p></h4></a>
             </div>
         </div>
-        <div class="col-sm-4">
-            <!-- <div class="button-main clearfix" data-toggle="modal" data-target=".bs-example-modal-lg">
-                <a href="" >
-                    <h4><p>Выбрать регион <span class="caret"></span></p></h4>
-                </a>
-            </div> -->
-        </div>
+        <!-- <div class="col-sm-1">
+        </div> -->
     </div>
-
-
-    <?php if ($events && $events->getCount()): ?>
-        <h2><p><?= Html::a('Мероприятия в коворкингах', ['event/index']) ?></p></h2>
-
-        <div class="row cr-events">
-            <?php $count = 1; ?>
-            <?php foreach ($events->getModels() as $event): ?>
-                <?php if ($count > 3) break; $url = Url::to(['event/view', 'id' => $event->id]); ?>
-                <div class="col-md-4">
-
-                    <h4><p><a href="<?=$url?>"><?=Html::encode("{$event->title}")?></a></p></h4>
-
-                    <?php
-                        $centerLogo = $event->centerLogo;
-                        if ($centerLogo) {
-                            echo '<div class="cr-events-logo">
-                                <a href="'.$url.'">
-                                    <img width=100% src="'.$centerLogo.'">
-                                </a>
-                            </div>';
-                        }
-                    ?>
-
-                    <p><?=Html::encode("{$event->anons_text}")?></p>
-
-                    <p><?= $event->eventDate ?></p>
-                    
-                </div>
-            <?php $count++; endforeach; ?>
-            <?php if ($count == 3)  echo '<div class="col-md-4"></div>';?>
-            <?php if ($count == 2)  echo '<div class="col-md-4"></div><div class="col-md-4"></div>';?>
-        </div>
-    <?php endif; ?>
 
 
     <h2><p><?= Html::a('Новости рынка', ['news/index']) ?></p></h2>
@@ -140,6 +105,38 @@ $this->params['showCounters'] = true;
         </div>
     </div>
 
+    <?php if ($events && $events->getCount()): ?>
+        <h2><p><?= Html::a('Мероприятия в коворкингах', ['event/index']) ?></p></h2>
+
+        <div class="row cr-events">
+            <?php $count = 1; ?>
+            <?php foreach ($events->getModels() as $event): ?>
+                <?php if ($count > 3) break; $url = Url::to(['event/view', 'id' => $event->id]); ?>
+                <div class="col-md-4">
+
+                    <h4><p><a href="<?=$url?>"><?=Html::encode("{$event->title}")?></a></p></h4>
+
+                    <?php
+                        $centerLogo = $event->centerLogo;
+                        if ($centerLogo) {
+                            echo '<div class="cr-events-logo">
+                                <a href="'.$url.'">
+                                    <img width=100% src="'.$centerLogo.'">
+                                </a>
+                            </div>';
+                        }
+                    ?>
+
+                    <p><?=Html::encode("{$event->anons_text}")?></p>
+
+                    <p><?= $event->eventDate ?></p>
+
+                </div>
+            <?php $count++; endforeach; ?>
+            <?php if ($count == 3)  echo '<div class="col-md-4"></div>';?>
+            <?php if ($count == 2)  echo '<div class="col-md-4"></div><div class="col-md-4"></div>';?>
+        </div>
+    <?php endif; ?>
 
     <h2><p><?= Html::a('Объявления', ['arenda/index'], ['class' => 'main-h3']) ?></p></h2>
 
